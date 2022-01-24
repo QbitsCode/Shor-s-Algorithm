9eeab9d49f3a381d3847c2a6a38b97f764f08e3f
evandrouzeda <evandrouzeda@gmail.com>
2022-01-24 13:20:58 +0000

    funcionando topermente

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ from qiskit.tools.visualization import plot_histogram
 
 msg = 9
 keys = CreateKeys.execute(1,30)
+print(keys.d)
+
+
 c = keys.encrypt(msg)
+print(c)
 
 backend = Aer.get_backend('qasm_simulator')
 quantum_instance = QuantumInstance(backend, shots=1000)
 my_shor = Shor(N=keys.n, a=2, quantum_instance=quantum_instance)
 
-Shor.run(my_shor)
-l = my_shor._get_factors()
-gkeys = GenerateKeysFromPublic.execute(l[0], l[1], keys.e, keys.n)
+result = Shor.run(my_shor)
+factors = result.get("factors")[0]
+print(factors)
 
+gkeys = GenerateKeysFromPublic.execute(factors[0], factors[1], keys.e, keys.n)
+print(gkeys.d)
 m = gkeys.decrypt(c)
+print(m)
