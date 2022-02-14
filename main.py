4eb9893157575335789b1ad59d5df37f16cb1744
evandrouzeda <evandrouzeda@gmail.com>
2022-02-14 14:33:08 +0000

    trocando para biclioteca qiskit-terra

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from sympy import N
 from RSA import CreateKeys, GenerateKeysFromPublic, PreSetPrimes
 
-from qiskit.aqua.algorithms import Shor
-from qiskit.aqua import QuantumInstance
+from qiskit.algorithms import Shor
+from qiskit.utils import QuantumInstance
 import numpy as np
 from qiskit import QuantumCircuit, Aer, execute
 from qiskit.tools.visualization import plot_histogram
 
 msg = 9
-# keys = CreateKeys.execute(1,30)
+#keys = CreateKeys.execute(1,15)
 keys = PreSetPrimes.execute(3, 7)
+#keys = PreSetPrimes.execute(3, 11)
 print(keys.d)
 
 
@@ -17,10 +19,12 @@ print(c)
 
 backend = Aer.get_backend('qasm_simulator')
 quantum_instance = QuantumInstance(backend, shots=1000)
-my_shor = Shor(N=keys.n, a=2, quantum_instance=quantum_instance)
+#my_shor = Shor(N=keys.n, a=2, quantum_instance=quantum_instance)
+my_shor = Shor(quantum_instance=quantum_instance)
 
-result = Shor.run(my_shor)
-factors = result.get("factors")[0]
+result = my_shor.factor(N=keys.n, a=2)
+print(result)
+factors = result._factors[0]
 print(factors)
 
 gkeys = GenerateKeysFromPublic.execute(factors[0], factors[1], keys.e, keys.n)
